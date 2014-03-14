0bd8f211e652e58a3ced694f32f504fb0543d559
panmari <smmuzi@gmail.com>
2014-03-14 20:20:01 +0100

    ex 4 solved with some issues left

---
+++ ex3/exercise_04.py
@@ -0,0 +1,24 @@
+
+import re
+import collections
+
+# this implementation tries to use fast data structures and prevents copying lists at all cost.
+p = re.compile('(\b.+)')
+
+with open('input_ex4.txt') as f:
+    # split() method splits on any whitespace by default
+    arry = f.read().split()
+    # turn arry into iterator with following filter
+    arry = filter(lambda x: not re.match(r'[<>\d]+', x), arry)
+    # TODO: possibly apply another filter or do splitting better to have
+    # [..., "end", ".", ...]
+    # instead of
+    # [..., "end.", ...]
+
+bigram_counter = collections.Counter()
+before = next(arry)
+for i in arry:
+    bigram_counter[(before, i)] += 1
+    before = i
+
+print(bigram_counter.most_common(10))
