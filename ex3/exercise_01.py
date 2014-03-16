0d8b36b5584cf313f20a6d659f448bf086b7b040
panmari <smmuzi@gmail.com>
2014-03-16 12:57:42 +0100

    adapted output of ex1 to statements on forum

--- ex3/exercise_01.py
+++ ex3/exercise_01.py
@@ -1,13 +1,23 @@
 
 import re
-tag_regex = re.compile("</?([^ ><]*) ?.*>")
+#ignores nodes with / and ? at the beginning
+tag_regex = re.compile("<([^?/][^ ><]*) ?.*>")
 tag_set = set()
+root = 0
 with open('input_ex1.txt') as f:
     for line in f:
         tags = tag_regex.search(line)
         if tags:
             for tag in tags.groups():
-                tag_set.add(tag)
+                # first tag found must be root
+                if root == 0:
+                    root = tag
+                else:
+                    tag_set.add(tag)
 
-print(tag_set)
-print(len(tag_set))
+print(root)
+print() #newline
+for tag in tag_set:
+    print(tag)
+print()
+print("Found altogether {} different nodes other than root node.".format(len(tag_set)))
