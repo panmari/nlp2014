e053f7b2a322dd17d114e8586f855938ad438765
panmari <smmuzi@gmail.com>
2014-03-16 14:29:13 +0100

    very cheap solution for ex2
    ...but it never says, we should do differently :P

--- ex3/exercise_02.py
+++ ex3/exercise_02.py
@@ -1,7 +1,22 @@
 
 # TODO: parse xml, throw exception if invalid
 import re
+docs = ['input_ex2_1.txt', 'input_ex2_2.txt', 'input_ex2_3.txt']
+
 
 # Idea: keep a stack of tags, check if they are closed in correct order
 # Idea: check if there are only allowed characters in tag
+
+
+
+
 # Idea: don't do this exercise and just use an xml parser, since there are already way too many of these to write a new one.
+import xml.etree.ElementTree as ET
+
+for doc in docs:
+    with open(doc) as f:
+        try:
+            t = ET.parse(f)
+            print("{}: YES".format(doc))
+        except Exception as e:
+            print("{}: NO, {}".format(doc, e))
