1a56021bc3995bca980c5febc70d34ade9ddbc6e
panmari <smmuzi@gmail.com>
2014-04-04 13:52:04 +0200

    sketched ex2

--- ex6/exercise_01.py
+++ ex6/exercise_01.py
@@ -1,12 +1,17 @@
 import re
 
-four_letter_words = set()
 
-with open('input_ex1.txt') as f:
-    for line in f.readlines():
-        words = re.findall('[ ^]([a-zA-Z]{4})[., \n]', line)
-        for word in words:
-            four_letter_words.add(word.strip())
+def get_four_letter_words():
+    four_letter_words = set()
+    with open('input_ex1.txt') as f:
+        for line in f.readlines():
+            words = re.findall('[ ^]([a-zA-Z]{4})[., \n]', line)
+            for word in words:
+                four_letter_words.add(word.strip())
+    return four_letter_words
 
-print('Found {} different four letter words:'.format(len(four_letter_words)))
-print(four_letter_words)
+# only print words if this script was called directly
+if __name__ == '__main__':
+    flw = get_four_letter_words()
+    print('Found {} different four letter words:'.format(len(flw)))
+    print(flw)
--- ex6/exercise_02.py
+++ ex6/exercise_02.py
@@ -1,1 +1,21 @@
-# check EVERYTHING
+from ex6.exercise_01 import get_four_letter_words
+import string
+import codecs
+en_dictionary = set()
+
+with codecs.open('english_dictionary.txt', encoding='iso-8859-15', mode='r') as f:
+    for line in f.readlines():
+        en_dictionary.add(line.strip())
+
+flw = get_four_letter_words()
+
+valid_constructed_flw = set()
+for word in flw:
+    for i in range(len(word)):
+        # checks substitution with upper- as well as lowercase letter.
+        for char in string.ascii_letters:
+            word_changed = word[:i] + char + word[i+1:]
+            if word_changed in en_dictionary:
+                valid_constructed_flw.add(word_changed)
+
+print(valid_constructed_flw)
