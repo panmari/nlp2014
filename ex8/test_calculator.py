5e00a0d8de35c2a387edf90bb2e827e4a39fa84c
panmari <smmuzi@gmail.com>
2014-05-02 16:39:31 +0200

    even more test cases

--- ex8/test_calculator.py
+++ ex8/test_calculator.py
@@ -3,6 +3,7 @@ __author__ = 'mazzzy'
 
 import unittest
 from exercise_05 import parse_calculator_string
+from pyparsing import ParseException
 
 class MyTestCase(unittest.TestCase):
     def test_simple_addtion(self):
@@ -15,13 +16,26 @@ class MyTestCase(unittest.TestCase):
         parsed = parse_calculator_string('1 * 2 + 3')
         self.assertEqual("['1', '*', '2', '+', '3']", str(parsed))
 
-    def test_multiple_terms(self):
-        parsed = parse_calculator_string('1 * 2 + 3')
-        self.assertEqual("['1', '*', '2', '+', '3']", str(parsed))
+    def test_variable_names(self):
+        parsed = parse_calculator_string('a * b')
+        self.assertEqual("['a', '*', 'b']", str(parsed))
 
     def test_multiplication_with_negative_number(self):
         parsed = parse_calculator_string('1 * -2 + 3')
         self.assertEqual("['1', '*', '-2', '+', '3']", str(parsed))
+
+    def test_starting_with_negative_number(self):
+        parsed = parse_calculator_string('-2 + 3')
+        self.assertEqual("['-2', '+', '3']", str(parsed))
+
+    def test_invalid_calc_string_times(self):
+        with self.assertRaises(ParseException):
+            parse_calculator_string(' * -2 + 3')
+
+    def test_invalid_calc_string_plus(self):
+        with self.assertRaises(ParseException):
+            parse_calculator_string(' + -2 + 3')
+
 
 if __name__ == '__main__':
     unittest.main()
